e896f00c74cf0f13548c78b420e3ccbb133f938c
ArtemBatkov <artembatkov2015@gmail.com>
2023-02-01 17:38:49 -0500

    Initial Commit

---
+++ Assignement_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignement_1
+//
+//  Created by user233573 on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
